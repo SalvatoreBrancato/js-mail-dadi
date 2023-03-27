2f0f889898d981d4dea272b6148aecbf91100322
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-03-27 23:12:31 +0200

    Struttura_definitiva

--- assets/main.js
+++ assets/main.js
@@ -9,10 +9,11 @@ function() {
         if (email == iscritti[i]) {
             soldatino = true;
             document.getElementById(`table`).style.display = "flex";
-            document.querySelector(`#accesso`).innerHTML = "<h2>Accesso consentito</h2>";
-        }
+            document.querySelector(`#accesso`).innerHTML = '<h2 class="text-white">Accesso consentito</h2>';
+            break;
+        } 
         else {
-            document.querySelector(`#accesso`).innerHTML = "<h2>Accesso negato</h2>";
+            document.querySelector(`#accesso`).innerHTML = '<h2 class="text-white">Accesso negato</h2>';
         }
     }
     // if (email == iscritti[0] || email == iscritti[1] || email == iscritti[2]) {
@@ -32,13 +33,13 @@ let dadoComputer = Math.floor(Math.random()*6)+1;
 document.getElementById("dadoGiocatore").innerHTML = dadoGiocatore;
 document.getElementById("dadoComputer").innerHTML = dadoComputer;
 if (dadoGiocatore > dadoComputer ) {
-    document.querySelector(".result").innerHTML = `Hai vinto!`;
+    document.querySelector(".result").innerHTML = `<h3 class="mt-5">Hai vinto!</h3>`;
 }
 else if (dadoGiocatore < dadoComputer ) {
-    document.querySelector(".result").innerHTML = `Hai perso!`;
+    document.querySelector(".result").innerHTML = `<h3 class="mt-5">Hai perso!</h3>`;
 }
 else {
-    document.querySelector(".result").innerHTML = ` Risultato pari`;
+    document.querySelector(".result").innerHTML = `<h3 class="mt-5">Risultato pari</h3>`;
 }
 });
 
